9131f3114f905493695fe37a2c7b7b701be1a17b
Nazar Votkanych <nazarvotkanych@gmail.com>
2024-02-29 02:48:43 +0200

    fixed minor issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,6 @@ import { StatusBar } from 'expo-status-bar';
 // import { weatherImages } from '../../src/constants/weatherImages';
 import { getData, storeData } from '../../src/utils/asyncStorage';
 import WeatherLocation from './WeatherLocation';
-import WeatherForecast from './WeatherForecast';
 import WeatherDetails from './WeatherDetails';
 
 export default function Home() {
@@ -194,9 +193,54 @@ export default function Home() {
                 Daily forecast
               </Text>
             </View>
-            <WeatherForecast
-              forecast={weather.forecast.forecastday}
-            />
+            <ScrollView
+              horizontal
+              contentContainerStyle={{ paddingHorizontal: 15 }}
+              showsHorizontalScrollIndicator={false}
+            >
+              {weather?.forecast?.forecastday?.map((item, index) => {
+                const date = new Date(item.date);
+                const options = { weekday: 'long' };
+                let dayName = date.toLocaleDateString('en-US', options);
+                dayName = dayName.split(',')[0];
+
+              return (
+                <View
+                  key={index}
+                  style={{
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: 110,
+                  paddingVertical: 10,
+                  paddingHorizontal: 15,
+                  borderRadius: 20,
+                  backgroundColor: theme.bgWhite(0.15),
+                  marginRight: 10 }}>
+                <Image
+                  source={{ uri: 'https:' + item?.day?.condition?.icon }}
+                  style={{
+                width: 50,
+                    height: 50
+                  }} />
+                <Text
+                  style={{
+                    color: 'white'
+                  }}>
+                    {dayName}
+                </Text>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontSize: 20,
+                    fontWeight: '600'
+                  }}>
+                    {item?.day?.avgtemp_c}&#176;
+                </Text>
+              </View>
+            );
+         })}
+    </ScrollView>
           </View>
         </SafeAreaView>
       )}
